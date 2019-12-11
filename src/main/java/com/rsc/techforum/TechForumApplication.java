e10d576b4d75f698c11520824fb5d52de0194308
Seb Carss <seb.carss@gmail.com>
2019-12-11 22:12:29 +0000

    Initial Spring Boot project created using start.spring.io

---
+++ src/main/java/com/rsc/techforum/TechForumApplication.java
@@ -0,0 +1,13 @@
+package com.rsc.techforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsc/techforum/TechForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsc.techforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
