8663f3fe6dfc2a73672ac51551a88c7ceaa69935
Seb Carss <seb.carss@gmail.com>
2019-12-11 22:43:23 +0000

    Add TechForumController and load into Spring context as RestController

--- src/test/java/com/rsc/techforum/TechForumApplicationTests.java
+++ src/test/java/com/rsc/techforum/TechForumApplicationTests.java
@@ -1,13 +1,23 @@
 package com.rsc.techforum;
 
+import com.rsc.techforum.controllers.TechForumController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class TechForumApplicationTests {
 
+	// TODO Review whether field autowiring is acceptable in tests and why
+	@Autowired
+	private TechForumController techForumController;
+
 	@Test
 	void contextLoads() {
+		assertThat(techForumController).isNotNull();
 	}
 
 }
